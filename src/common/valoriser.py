e436bd601d42fae426afd7ee947b40c0c3706956
sebastian borquez <sebastian.borquez@gmail.com>
2018-04-14 12:17:19 -0300

    Agregado clase en python

---
+++ src/common/valoriser.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+class Valoriser(object):
+    """
+    Valoriser se encarga de obtener los datos desde fuentes externas o locales
+    sanitisarlos y luego efectuar el calculo sobre ellos.
+    """
+
+    def __init__(self):
+        pass
+
+    def download(self):
+        pass
+
+    def load(self):
+        pass
+
+    def eval(self):
+        # This one is spooky
+        pass
