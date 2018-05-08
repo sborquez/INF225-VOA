8de7b7c4c07fdc4f6b5efe5a8577305a1de956b4
sborquez <sebastian.borquez@gmail.com>
2018-05-08 20:46:24 -0300

    European and American option

--- src/common/options.py
+++ src/common/options.py
@@ -31,25 +31,18 @@ class OptionPricing(object):
         
         if T == 0:
             T = self.time_to_maturity
+        
+        # using time in years
+        T /= 365.0
+
         if simulations == 0:
             simulations = self.I
-
-        # if self.pc == 'call':
-        #     calc[:,1] = mult*np.exp(np.sqrt((self.sigma**2)*self.T)*rand) - self.K
-        # elif self.pc == 'put':
-        #     calc[:,1] = self.K - mult*np.exp(np.sqrt((self.sigma**2)*self.T)*rand)
- 
-        # avg_po = np.sum(np.amax(calc, axis=1))/float(self.iterations)
- 
-        # return np.exp(-1.0*self.r*self.T)*avg_po
 
         # Get an array of random values X~N(0,1), it'll be used to generate the Monte Carlo simulations
         simulations = np.random.normal(0, 1, simulations)
 
-
         # The price simulated at time T, with risk less rate of r and a volatility sigma
         # S(t) = S(0) * exp((r - 0.5*sigma**2)*T  + sigma*X*sqrt(T))
-
         prices = self.initial_price*np.exp(T*(self.riskless_rate - 0.5*self.volatility**2) + self.volatility * (T**.5) * simulations)
 
         # prices can't be negative
@@ -71,13 +64,15 @@ class OptionPricing(object):
         # T is the vector of times steps, reshaped to a vertical vector.
         T = np.linspace(0, T, T//steps).reshape((T//steps, 1))
 
+        # using time in years
+        T /= 365.0 
+
         # Get a matrix of random values X~N(0,1), 
         # it'll be used to generate the Monte Carlo simulations at differents times.
         simulations = np.random.normal(0, 1, [T.size , simulations])
         
         # The price simulated at time T, with risk less rate of r and a volatility sigma
         # S(t) = S(0) * exp((r - 0.5*sigma**2)*T  + sigma*X*sqrt(T))
-
         prices = self.initial_price*np.exp(T*(self.riskless_rate - 0.5*self.volatility**2) + self.volatility * np.sqrt(T) * simulations)
 
         # prices can't be negative
@@ -104,18 +99,34 @@ class OptionPricing(object):
 
 
 class EuropeanOptionPricing(OptionPricing):
-    #TODO    
+    """
+    EuropeanOptionPricing is european style option.
+    A European option may be exercised only at the expiration
+    date of the option, at a single pre-defined point in time T.
+    """
     def getCallOption(self):
-        raise NotImplementedError
-
+        t = self.time_to_madurity/365.0
+        payoff = np.mean(self.simulatePrices()) - self.strike_price
+        return np.exp(-1.0 * self.riskless_rate * t) * payoff
+        
     def getPullOption(self):
-        raise NotImplementedError
+        t = self.time_to_madurity/365.0        
+        payoff = self.strike_price - np.mean(self.simulatePrices())
+        return np.exp(-1.0 * self.riskless_rate * t) * payoff
 
 
-class AmericanOptionPricing(OptionPricing): 
-    #TODO   
-    def getCallOption(self):
-        raise NotImplementedError
-
-    def getPullOption(self):
-        raise NotImplementedError
+class AmericanOptionPricing(OptionPricing):
+    """
+    AmericanOptionPricing
+    An American option may be exercised at any time before 
+    the expiration date, that is from t in (0,T).
+    """
+    def getCallOption(self, steps=1):
+        T = np.linspace(0, 1, self.time_to_madurity//steps)*(self.time_to_madurity/365.0)
+        payoff = np.mean(self.simulatePrices(), axis=1) - self.strike_price
+        return np.exp(-1.0 * self.riskless_rate * T) * payoff
+        
+    def getPullOption(self, steps=1):
+        T = np.linspace(0, 1, self.time_to_madurity//steps)*(self.time_to_madurity/365.0)
+        payoff = self.strike_price - np.mean(self.simulatePrices(), axis=1)
+        return np.exp(-1.0 * self.riskless_rate * T) * payoff
